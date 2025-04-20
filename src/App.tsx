f496703849244913763d46224f21b8acbae3c505
chenjunCodingGit <271676725@qq.com>
2025-04-20 17:36:33 +0800

    feat: added the chatbot ui framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Chat, { Bubble, useMessages } from '@chatui/core';
+import chatbotAvatar from './assets/chatbot-avatar.svg';
 
-function App() {
-  const [count, setCount] = useState(0)
+const initialMessages = [
+  {
+    type: 'system',
+    content: { text: 'Chatbot AI at your service.' },
+  },
+  {
+    type: 'text',
+    content: { text: 'Hi, I am your exclusive intelligent assistant. Please feel free to contact me if you have any questions.' },
+    user: {
+      avatar: chatbotAvatar,
+    },
+  },
+];
+
+const defaultQuickReplies = [
+  {
+    icon: 'message',
+    name: 'Manual service',
+    isNew: true,
+    isHighlight: true,
+  },
+  {
+    name: 'Custom Question 1',
+    isNew: true,
+  },
+  {
+    name: 'Custom Question 2',
+    isHighlight: true,
+  },
+  {
+    name: 'Custom Question 3',
+  },
+];
+
+export default function() {
+  const { messages, appendMsg } = useMessages(initialMessages);
+
+  // 发送回调
+  function handleSend(type: string, val: string) {
+    if (type === 'text' && val.trim()) {
+      appendMsg({
+        type: 'text',
+        content: { text: val },
+        position: 'right',
+      });
+
+      // TODO: 发送请求
+      // 模拟回复消息
+      setTimeout(() => {
+        appendMsg({
+          type: 'text',
+          content: { text: 'Please briefly describe your problem.' },
+        });
+      }, 1000);
+    }
+  }
+
+  // 快捷短语回调，可根据 item 数据做出不同的操作，这里以发送文本消息为例
+  function handleQuickReplyClick(item: { name: any; }) {
+    handleSend('text', item.name);
+  }
+
+  function renderMessageContent(msg: { type: any; content?: any; }) {
+      const { type, content } = msg;
+  
+      // 根据消息类型来渲染
+      switch (type) {
+        case 'text':
+          return content?.text ? <Bubble content={content.text} /> : null;
+        case 'image':
+          return content?.picUrl ? (
+            <Bubble type="image">
+              <img src={content.picUrl} alt="" />
+            </Bubble>
+          ) : null;
+        default:
+          return null;
+      }
+    }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Chat
+    locale="en-US"
+      navbar={{ title: 'ChatBot AI' }}
+      messages={messages}
+      renderMessageContent={renderMessageContent}
+      quickReplies={defaultQuickReplies}
+      onQuickReplyClick={handleQuickReplyClick}
+      onSend={handleSend}
+    />
+  );
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import '@chatui/core/dist/index.css';
+import './styles/chatui-theme.css'
 
 const {OmnichannelChatSDK_1: OmnichannelChatSDK } = window as any;
 console.log('OmnichannelChatSDK: ', OmnichannelChatSDK);
